636d9673313a4ccb770ed0478dfba377650cfd49
itzkumaran <kumaran4jobs@gmail.com>
2024-05-12 16:44:06 -0400

    fx

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -19,7 +19,7 @@ export const ProfileData = {
   heading: 'Professional Profile',
   data: [
     'Professional level Cloud/DevOps architect with Software Engg. - 19 years of IT experience - DataCenter => Cloud Migration, DevOps, SRE',
-    'Multi-skilled in various stacks AWS, Azure, Kubernetes, Terraform, Packer, Vault, Gitlab, Cloud Security, Networking, HPC, ITIL, Scrum, SDLC',
+    'Multi-skilled in various stacks - AWS, Azure, Kubernetes, Terraform, Packer, Vault, Gitlab, Cloud Security, Networking, HPC, ITIL, Scrum Methodologies, SDLC',
     'Have a very good experience in Identity Services & Backend Python development',
     'Seeking a challenging role in a dynamic organization where I can apply my skills and grow along with the company & team',
   ],
@@ -30,13 +30,21 @@ export const ProfileData = {
 export const SkillsData = {
   heading: 'Skills & Technologies',
   data: [
-    'Proficient in Angular, JavaScript, HTML, CSS, and TypeScript.',
-    'Experience in developing responsive web applications.',
-    'Knowledge of version control tools like Git.',
-    'Proficient in Python programming language.',
-    'Knowledge of Machine Learning concepts and techniques.',
-    'Familiarity with OpenCV library for computer vision tasks.',
-    'Familiarity with database management systems like MySQL and MongoDB.',
+    'Proficient in migrating applications and infrastructure from on-premises data center to cloud',
+    'Proficient in writing Terraform scripts, modules for managing resources across different platforms - AWS, Azure, Kubernetes, NewRelic, VMWare',
+    'Proficient in automating configuration management across 100s of servers both on-premises & cloud',
+    'Proficient in automating patch management across 100s of servers in AWS using Systems Manager (SSM)',
+    'Proficient in building backend services & APIs using Python',
+    'Proficient in writing Dockerfiles & publishing images to JFrog Artifactory',    
+    'Proficient in building K8s clusters from scratch and configure RBAC for securing them',
+    'Proficient in setting up Azure Entra ID & configuring PIM workflows',
+    'Proficient in setting up Okta tenants using Terraform & securing them',
+    'Proficient in setting up Saviynt for Governance and Audits of the privilaged application/infra roles',    
+    'Knowledge of operational management of Slurm On-Premises clusters (HPC)',
+    'Proficient in automating repetitive maintenance tasks using Powershell, Bash & Python',    
+    'Basic Knowledge of Machine Learning concepts and techniques.',
+    'Basic Knowledge of Snowflake and data pipelines',
+    'Familiarity with database management systems like MySQL and Oracle',
     'Familiarity with Agile methodologies and Scrum framework.',
   ],
 };
@@ -47,7 +55,7 @@ export const WorkData = {
   heading: 'Work Experience',
   data: [
     {
-      role: 'Cloud Architect',
+      role: 'Architect - Cloud, Identity & Platform Services',
       date: 'May, 2019 - Present',
       company: 'Acadian Asset Management',
       location: 'Boston, MA USA',
@@ -61,10 +69,10 @@ export const WorkData = {
       ],
     },
     {
-      role: 'Angular Developer | Frontend Developer | Assistant System Engineer',
-      date: 'October 2020 - August 2022',
-      company: 'TATA Consultancy Services',
-      location: 'Kochi, Kerala, India',
+      role: 'Senior DevOps Engineer',
+      date: 'May, 2018 - Apr, 2019',
+      company: 'Wellington Management (C)',
+      location: 'Boston, MA USA',
       works: [
         'Worked as an Angular developer on the GE HealthCare project entitled Digitized Problem-Solving Report (dPSR).',
         'Implemented a single-page application (SPA) using Angular 10, TypeScript, JavaScript, and SASS.',
